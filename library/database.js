ad333dffa28843ac5b4c584e61fec6796520cbf7
Farrel Ardian <farrelardian123@gmail.com>
2024-07-13 05:00:05 +0700

    Update database.js

--- library/database.js
+++ library/database.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "q78.h.filess.io",
-  user: "mebel_barepoetif",
-  password: "40e51090cc4719d5c883feebb220303d3893a98b",
-  database: "mebel_barepoetif",
-  port: "3307",
+  host: "sql12.freesqldatabase.com",
+  user: "sql12719487",
+  password: "FeRJAjM5YR",
+  database: "sql12719487",
+  port: "3306",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
