b04633a4fa7242edbf3067da309dc3b368ad7796
Farrel Ardian <farrelardian123@gmail.com>
2024-07-14 23:58:42 +0700

    fix

--- library/database.js
+++ library/database.js
@@ -1,11 +1,10 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "sql12.freesqldatabase.com",
-  user: "sql12719487",
-  password: "FeRJAjM5YR",
-  database: "sql12719487",
-  port: "3306",
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mebel_kampus",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
